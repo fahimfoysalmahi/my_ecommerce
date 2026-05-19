04db04338bfc8ebfa39a52e53eb977c0eb8f944b
fahimfoysalmahi <fahim33862@gmail.com>
2026-05-19 15:25:43 +0600

    Update models.py

--- my_ecommerce/models.py
+++ my_ecommerce/models.py
@@ -1,21 +1,15 @@
 from django.db import models
 
 class Product(models.Model):
-    # ক্যাটাগরির জন্য অপশন
-    CATEGORY_CHOICES = [
-        ('electronics', 'Electronics'),
-        ('calculators', 'Calculators'),
-        ('mobiles', 'Mobiles'),
-        ('others', 'Others'),
-    ]
-
     name = models.CharField(max_length=200)
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='others')
-    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    original_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True) # অফার দেখানোর জন্য
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
-    in_stock = models.BooleanField(default=True) # স্টক আছে কি না
+    original_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True) # এই লাইনটা লাগবেই
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    category = models.CharField(max_length=100, blank=True, null=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     def __str__(self):
         return self.name

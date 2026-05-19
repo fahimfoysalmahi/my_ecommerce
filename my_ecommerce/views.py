da158351f1db62e0c49ee9623c8eb5e0310c254b
fahimfoysalmahi <fahim33862@gmail.com>
2026-05-19 15:36:27 +0600

    Update views.py

--- my_ecommerce/views.py
+++ my_ecommerce/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
-# হোমপেজের ভিউ ফাংশন
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
 
-# প্রোডাক্ট ডিটেইলস পেজের ভিউ ফাংশন
 def product_detail(request, id):
     product = get_object_or_404(Product, id=id)
     return render(request, 'product_detail.html', {'product': product})

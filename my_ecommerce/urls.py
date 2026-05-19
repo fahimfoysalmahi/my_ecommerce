4aa761d2fbf4a11e353fe6f488d6d744ae84c023
fahimfoysalmahi <fahim33862@gmail.com>
2026-05-19 13:54:28 +0600

    Update urls.py

--- my_ecommerce/urls.py
+++ my_ecommerce/urls.py
@@ -2,17 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
-
-# একটা সাময়িক টেস্ট ভিউ বানিয়ে নিলাম
-def temporary_home(request):
-    return HttpResponse("<h1>আলহামদুলিল্লাহ! ফয়সালের জ্যাঙ্গো ওয়েবসাইট সাকসেসফুলি লাইভ হয়েছে!</h1>")
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', temporary_home, name='home'),
+    path('', views.home, name='home'),
+    path('product/<int:id>/', views.product_detail, name='product_detail'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    

85677c34f8579695604b08fd9303f24c0e6196ef
fahimfoysalmahi <fahim33862@gmail.com>
2026-05-19 16:09:06 +0600

    Update urls.py

--- my_ecommerce/urls.py
+++ my_ecommerce/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth.models import User
 from . import views
 
-# একটা অটোমেটিক সুপারইউজার তৈরি করার ট্রিক
+# অটোমেটিক সুপারইউজার ট্রিক
 try:
     if not User.objects.filter(username='admin').exists():
         User.objects.create_superuser('admin', 'admin@example.com', 'foysal1234')
@@ -15,3 +17,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('product/<int:id>/', views.product_detail, name='product_detail'),
 ]
+
+# লাইভ সার্ভারে প্রোডাক্টের ছবি দেখানোর রাস্তা জুঁড়ে দেওয়া হলো
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

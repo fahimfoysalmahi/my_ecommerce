a69f5ad7bf47fd944edcf9e4964c64af4652e6fb
fahimfoysalmahi <fahim33862@gmail.com>
2026-05-19 15:33:59 +0600

    Update models.py

--- my_ecommerce/models.py
+++ my_ecommerce/models.py
@@ -3,13 +3,10 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    original_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True) # এই লাইনটা লাগবেই
+    original_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
     category = models.CharField(max_length=100, blank=True, null=True)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
     def __str__(self):
         return self.name
